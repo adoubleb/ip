10687617a376da476d6a1041c5bf7d8b2e0ba504
adoubleb <hakam.9@outlook.com>
2025-01-19 22:25:15 +0800

    Changed Duke.java to Brownie.java and edited text

---
+++ src/main/java/Brownie.java
@@ -0,0 +1,6 @@
+public class Brownie {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Brownie \nWhat can I do for you?" +
+                "\nBye. Hope to see you again soon!");
+    }
+}
