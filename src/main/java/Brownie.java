1202e8be0d13eb7b30873f976b32020c3d7cca72
adoubleb <hakam.9@outlook.com>
2025-01-20 15:33:13 +0800

    Brownie echoes

--- src/main/java/Brownie.java
+++ src/main/java/Brownie.java
@@ -1,6 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
 public class Brownie {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Brownie \nWhat can I do for you?" +
-                "\nBye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Brownie \nWhat can I do for you?");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter writer = new PrintWriter(System.out);
+        while (true) {
+            String input = null;
+            try {
+                input = reader.readLine();
+                writer.println(input);
+                writer.flush();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (input.equals("bye")) {
+                writer.println("Bye. Hope to see you again soon!");
+                writer.flush();
+                break;
+            }
+        }
+        writer.close();
     }
 }
