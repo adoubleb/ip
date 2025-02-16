27b419060a57d115774372e246867c2162e6e88d
adoubleb <hakam.9@outlook.com>
2025-02-16 15:12:17 +0800

    Format deadline datetime output

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -26,6 +26,7 @@ public class Main extends Application {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
+            stage.setTitle("Brownie");
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setBrownie(brownie);
